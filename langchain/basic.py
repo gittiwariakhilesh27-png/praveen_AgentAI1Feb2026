e9a1eae69772815098c11f9baf4e47b2b203d812
admin <praveen.trainer@outlook.com>
2026-02-07 04:26:20 +0000

    Runnables

--- langchain/basic.py
+++ langchain/basic.py
@@ -7,7 +7,7 @@ from langchain_openai import ChatOpenAI
 
 from langchain_core.prompts import PromptTemplate, ChatPromptTemplate
 from langchain_core.output_parsers import StrOutputParser
-# from langchain_core.runnables import RunnablePassthrough
+from langchain_core.runnables import RunnablePassthrough
 
 from dotenv import load_dotenv
 
@@ -43,7 +43,9 @@ def demo_chat_prompt():
 
     chat_prompt = ChatPromptTemplate.from_messages(
         [
+            # LLM Persona
             ("system", "You are a {role} who speaks in a {style} manner."),
+            # User Question
             ("human", "{user_input}"),
         ]
     )
@@ -54,14 +56,37 @@ def demo_chat_prompt():
         {
             "role": "Hindi Teacher",
             "style": "Replies only in Hindi",
-            "user_input": "Tell me about the benefits of Python programming",
+            "user_input": "Tell me about the benefits of Python programming and what is my name and where i am from",
         }
     )
 
+    print(result)
+
+def demo_sequential():
+   
+    topic_prompt = PromptTemplate.from_template(
+        "Generate a creative topic about {subject} in one sentence."
+    )
+    content_prompt = PromptTemplate.from_template(
+        "Write a short paragraph (2-3 sentences) about: {topic}"
+    )
+
+    topic_runnable = topic_prompt | llm | StrOutputParser()
+    content_runnable = content_prompt | llm | StrOutputParser()
+
+    sequential = (
+        RunnablePassthrough()
+        .assign(topic=topic_runnable)
+        .assign(content=content_runnable)
+    )
+
+    result = sequential.invoke({"subject": "marvels movie: Ironman 2"})
+    
     print(result)
 
 
 
 if __name__ == "__main__":
     # demo_basic_prompt()
-    demo_chat_prompt()
+    #demo_chat_prompt()
+    demo_sequential()
